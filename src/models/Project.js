a77232b6feaaa33f4acf7201c78409f585aab36b
ini080 <ini080@naver.com>
2019-08-23 11:33:17 +0900

    image 추가

--- src/models/Project.js
+++ src/models/Project.js
@@ -1,10 +1,12 @@
+const root = '@/assets/image/project/'
+
 export default [
   {
     'name' : '아두이노 자동주차 시스템',
     'info' : '',
     'disc' : '아두이노와 RC카를 결합하고, 자동으로 주차를 도와주는 시스템입니다.',
     'lang':'C언어',
-    'img' : '',
+    'img' : root + 'AutoParking.jpg',
     'date' : '2018.07',
     'link' : 'https://github.com/ini080/Arduino_AutoParking',
   },
@@ -13,7 +15,7 @@ export default [
     'info' : 'Springboot + Mysql + Mybatis + Vue.js',
     'disc' : '안전한 먹거리를 제공하는 프로젝트입니다.',
     'lang':'C언어',
-    'img' : '',
+    'img' : root + 'SmartBlind.png',
     'date' : '2018.11',
     'link' : 'https://github.com/ini080/Arduino_SmartBlind',
   },
@@ -22,7 +24,7 @@ export default [
     'info' : 'Springboot + Mysql + Mybatis + Vue.js',
     'disc' : '안전한 먹거리를 제공하는 프로젝트입니다.',
     'lang':'Java, Springboot, Vue.js, Mybatis, Mysql',
-    'img' : '',
+    'img' : root + 'SafeFood.jpg',
     'date' : '2019.05',
     'link' : 'https://github.com/ini080/Safe_Food',
   },
@@ -31,7 +33,7 @@ export default [
     'info' : 'Springboot + Mysql + Mybatis + Vue.js',
     'disc' : 'Vue.js로 만든 글과 댓글을 작성할 수 있는 프로젝트',
     'lang':'Java, Springboot, Firebase, Vue.js',
-    'img' : '',
+    'img' : root + 'WebMobile.PNG',
     'date' : '2019.08',
     'link' : 'https://github.com/ini080/web-Mobile-Final',
   },
@@ -40,7 +42,7 @@ export default [
     'info' : 'Node JS + API + KAKAO I OpenBuilder',
     'disc' : '여러 API를 사용하여 원하는 대답을 해주는 카카오톡 챗봇입니다.',
     'lang':'Node.Js , Firebase',
-    'img' : '',
+    'img' : root + 'ChatBot.jpg',
     'date' : '2019.07',
     'link' : 'https://github.com/ini080/chatbot'
   },
