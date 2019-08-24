f77d4606d381ea1f158c3ce076ddacd96daa199b
jg <ini080@naver.com>
2019-08-25 00:28:26 +0900

    delete all file

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/dist' : '.',
-  chainWebpack: config => {
-    config.module
-          .rule('svg')
-          .use('file-loader')
-          .loader('vue-svg-loader')
-  }
-}
