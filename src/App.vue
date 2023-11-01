54b35e2858f1a0234a63b7313c706b10e135f281
ini080 <ini080@naver.com>
2023-11-01 15:34:18 +0900

    add lockpage.vue

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,56 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div id="app">
+    <transition name="fade" mode="out-in">
+      <router-view
+        @onLogin="changeRoute('Home')"
+        @onLogout="changeRoute('Lock')">
+      </router-view>
+    </transition>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+
+<script>
+export default {
+  name: 'app',
+  methods: {
+    changeRoute (routeName) {
+      this.$router.push({ name: routeName })
+    }
+  }
+}
+</script>
+
+
+<style>
+
+* {
+  scroll-behavior: smooth;
+  -webkit-user-drag: none;
+  -khtml-user-drag: none;
+  -moz-user-drag: none;
+  -o-user-drag: none;
+  user-drag: none;
+
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+html, body {
+  height: 100%;
+  overflow: hidden;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+  color: #2c3e50;
+  height: 100%;
 }
 </style>
---
+++ src/views/LockPage.vue
@@ -0,0 +1,236 @@
+<template>
+  <div class="lockPage" @click="lockToggle">
+    <transition name="lockAni" >
+      
+      <!-- 잠금화면 -->
+      <div v-if="lock" class="modal">
+          <div class="time_area"> {{ time }} </div>
+          <div class="date_area"> {{ date }} </div>
+      </div>
+
+      <!-- 로그인시도 화면-->
+      <div v-else class="modal">
+          <!-- 애니메이션 -->
+          <div class="backdrop"></div>
+          
+          <div class="Login_area" >
+              <div class="userLogo"><img src="../assets/login_icon.png" /></div>
+
+              <div class="userName" v-if="!login">
+                  <h1>JeongGeon Heo</h1>
+              </div>
+
+              <transition name="fade" mode="out-in">
+                  <button class="userLogin_button" id="loginBtn" @click="onLogin" v-if="!login">
+                      Login
+                  </button>
+                  <div class="userLogin_message" v-else>환영합니다</div>
+              </transition>
+          </div>
+      </div>
+
+    </transition>
+  </div>
+</template>
+
+<script>
+import Format from '@/store/date-format.js'
+
+export default {
+  data() {
+    return {
+      lock: true,
+      login: false,
+      format: Format,
+      time: Format.getTime(null, 'en'),
+      date: Format.getDate(null, 'kr'),
+      ap: Format.getAp(null, 'kr')
+    };
+  },
+
+  created() {
+      this.refreshTime();
+  },
+
+  methods: {
+      lockToggle(){
+          if( this.login ) return;
+          this.lock = !this.lock
+      },
+      refreshTime() { // 시간과 날짜 1초마다 갱신
+          setInterval(() => {
+              this.time = this.format.getTime(null, 'en')
+              this.date = this.format.getDate(null, 'kr')
+              this.ap = this.format.getAp(null, 'kr')
+          }, 1000)
+      },
+
+      onLogin(e) {
+          e.stopPropagation()
+          this.login = true
+          setTimeout(() => {
+              this.$emit('onLogin')
+          }, 3000)
+      },
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  margin: 0;
+}
+.lockPage {
+width: 100vw;
+height: 100vh;
+background: url("../assets/windows_background.jpg") center;
+background-size: cover;
+display: flex;
+justify-content: center;
+align-items: center;
+}
+
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+
+  > .backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  backdrop-filter: blur(4px);
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: -1;
+  }
+}
+
+.lockAni-enter-active,
+.lockAni-leave-active {
+  transition: opacity 0.5s;
+}
+.lockAni-enter,
+.lockAni-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+transition: opacity 0s;
+}
+.fade-enter,
+.fade-leave-to {
+opacity: 0;
+}
+
+.time_area {
+  position: absolute;
+  bottom: 0;
+  margin: 20px;
+  color: #fff;
+  font-size: 6em;
+  font-family: D2;
+  top: 65vh;
+  font-weight: bold;
+  padding: 10px 20px;
+}
+
+.date_area {
+  position: absolute;
+  bottom: 0;
+  margin: 0;
+  color: #fff;
+  font-size: 4em;
+  font-family: D2;
+  top: 65vh;
+  padding: 150px 40px;
+}
+
+.Login_area {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .userLogo {
+      width: 15em;
+      height: 15em;
+      position: absolute;
+      top: 35%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+
+      @media screen and (max-width:599px) {
+      top: 25%;
+      }
+  }
+
+  .userLogo img {
+      width: 15em;
+      height: 15em;
+      border-radius: 10em;
+
+      @media screen and (max-width:599px) {
+      width: 65%;
+      height: 65%;
+      }
+  }
+
+  .userName {
+      position: absolute;
+      top: 51%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      text-align: center;
+      color: #fff;
+      @media screen and (max-width:599px) {
+          top: 14%;
+          font-size: 0.8em;
+      }
+  }
+
+  .userLogin_button {
+      position: absolute;
+      top: 56.5%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      cursor: pointer;
+      border: 1px solid #fff;
+      border-radius: 5px;
+      background-color: rgba(0, 0, 0, 0);
+      outline: none;
+      color: #fff;
+      font-weight: bold;
+      padding: 10px 20px;
+      transition: 0.3s;
+
+      @media screen and (max-width:599px) {
+          top: 100px;
+      }
+  }
+
+  .userLogin_button:hover {
+      background-color: rgba(255, 255, 255, 0.4);
+      color: #eee;
+  }
+
+  .userLogin_message {
+      position: absolute;
+      top: 53%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      text-align: center;
+      font-weight: bold;
+      font-size: 2rem;
+      color: #fff;
+
+      @media screen and (max-width:599px) {
+          top: 130px;
+          font-size: 1.5rem;
+      }
+  }
+}
+</style>
