6461c5b38636db56dc4b4bc6723204f44414a180
ini080 <ini080@naver.com>
2023-11-02 00:10:12 +0900

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,20 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{vue,js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+module.exports = {
+  purge: {content: ['./public/**/*.html', './src/**/*.vue']},
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+      extend: {
+          fontSize: {
+              '2xs': ['0.65rem', '0.8rem'],
+          },
+      },
+  },
+  variants: {
+      extend: {
+          scale: ['responsive', 'hover', 'focus', 'active', 'group-hover'],
+          backgroundColor: ['active'],
+          backgroundOpacity: ['active'],
+      },
+  },
+  plugins: [],
+}
+
