5b28e0392ef744d6293fa2e833fe7585ef550036
JG <jg@JGs-MacBook-Pro.local>
2023-11-09 13:37:31 +0900

    LockPage 반응형 적용

--- src/views/LockPage.vue
+++ src/views/LockPage.vue
@@ -4,31 +4,36 @@
       
       <!-- 잠금화면 -->
       <div v-if="lock" class="fixed bottom-0 left-0">
-        <p class=" text-white p-4 m-2 text-left text-4xl
+        <p class=" text-white p-4 m-2 text-left
                   sm:p-2 sm:pt-0 sm:m-1 sm:text-2xl
-                  md:p-4 md:pt-0 md:m-2
-                  lg:p-2 lg:m-1">{{ date }}</p>
+                  md:p-4 md:pt-0 md:m-2 mg:text-3xl
+                  lg:p-2 lg:m-1 lg:ml-10 lg:text-4xl">{{ date }}</p>
         <p class="text-white p-4 pt-0 m-2 mb-12 text-left font-bold
                 sm:p-2 sm:pt-0 sm:m-1 sm:mb-10 sm:text-6xl
                 md:p-4 md:m-2 md:w-1/2 md:text-4xl
-                lg:p-2 lg:m-1 lg:w-1/3 lg:text-4xl">{{ time }}</p>
+                lg:p-2 lg:m-1 lg:pt-0 lg:ml-10 lg:mb-10 lg:w-1/3 lg:text-8xl">{{ time }}</p>
       </div>
       
       <!-- 로그인시도 화면-->
-      <div v-else class="fixed">
+      <div v-else class="fixed loginPage">
           <!-- 애니메이션 -->
-          <div class="fixed backdrop-blur-sm bg-[rgba(0, 0, 0, 0.6)] z-[-1]"></div>
+          <div class="backdrop"></div>
           
           <div class="absolute justify-center items-center">
-            <div class="w-60 h-60 absolute top-3/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <div class="w-60 fixed top-1/3 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               <img src="@/assets/login_icon.png" alt="User Logo" class="w-full h-full rounded-full">
             </div>
-            <div v-if="!login" class="absolute top-51 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white font-bold text-2xl">
-              User Name
-            </div>
-            <button class="absolute top-56.5 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer border border-white rounded-md bg-transparent outline-none text-white font-bold py-1 px-10 transition duration-300">
-              Login
-            </button>
+            
+            <p v-if="!login" class="absolute w-60 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white font-bold text-3xl" >
+                JeongGeon Heo
+            </p>
+
+            <transition name="fade" mode="out-in">
+              <button v-if="!login" class="absolute top-14 left-1/2 transform -translate-x-1/2 -translate-y-1/2 cursor-pointer border border-white rounded-md outline-none text-white font-bold py-1 px-10" @click="onLogin">
+                Login
+              </button>
+              <div class="absolute w-60 top-4 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-3xl" v-else>환영합니다</div>
+            </transition>
           </div>
           
           <!-- <div class="Login_area" >
@@ -86,9 +91,7 @@ export default {
       onLogin(e) {
           e.stopPropagation()
           this.login = true
-          setTimeout(() => {
-              this.$emit('onLogin')
-          }, 3000)
+          setTimeout(() => { this.$emit('onLogin') }, 3000)
       },
   }
 };
@@ -107,7 +110,7 @@ export default {
 
 .fade-enter-active,
 .fade-leave-active {
-transition: opacity 0s;
+transition: opacity 0.5s;
 }
 .fade-enter,
 .fade-leave-to {
@@ -118,75 +121,17 @@ opacity: 0;
   background-image: url('../assets/windows_background.jpg');
 }
 
-.Login_area {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  .userLogo {
-      width: 15em;
-      height: 15em;
-      position: absolute;
-      top: 35%;
-      left: 50%;
-      transform: translate(-50%,-50%);
-
-  }
-
-  .userLogo img {
-      width: 15em;
-      height: 15em;
-      border-radius: 10em;
-
-  }
-
-  .userName {
-      position: absolute;
-      top: 51%;
-      left: 50%;
-      transform: translate(-50%,-50%);
-      text-align: center;
-      color: #fff;
-      font-weight: bold;
-      font-size: 25px;
-  }
-
-  .userLogin_button {
-      position: absolute;
-      top: 56.5%;
-      left: 50%;
-      transform: translate(-50%,-50%);
-      cursor: pointer;
-      border: 1px solid #fff;
-      border-radius: 5px;
-      background-color: rgba(0, 0, 0, 0);
-      outline: none;
-      color: #fff;
-      font-weight: bold;
-      padding: 5px 40px;
-      transition: 0.3s;
-
-  }
-
-  .userLogin_button:hover {
-      background-color: rgba(255, 255, 255, 0.4);
-      color: #eee;
-  }
-
-  .userLogin_message {
-      position: absolute;
-      top: 53%;
-      left: 50%;
-      transform: translate(-50%,-50%);
-      text-align: center;
-      font-weight: bold;
-      font-size: 2rem;
-      color: #fff;
-
-      @media screen and (max-width:599px) {
-          top: 130px;
-          font-size: 1.5rem;
-      }
+// blur 및 애니메이션을 위해 tailwindcss 미적용
+.loginPage{
+  > .backdrop {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  backdrop-filter: blur(4px);
+  background-color: rgba(0, 0, 0, 0.6);
+  z-index: -1;
   }
 }
 </style>
